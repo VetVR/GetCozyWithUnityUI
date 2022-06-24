8f2b9274e44088dbc17a379a26d161ff566ca8bb
Ron H <ronhat@gmail.com>
2022-06-24 10:09:04 -0700

    Finally got Course provided resources added.

---
+++ Assets/GettingCozy_UI_Video_Assets/Scripts/GT_ScreenManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GT_ScreenManager : MonoBehaviour
+{
+	#region Public Variables
+	public GameObject optionsScreen;
+	public GameObject labScreen;
+	public GameObject startScreen;
+	#endregion
+
+	#region Private Variables
+	private int curScreenID = 0;
+	private Animator curAnim;
+	#endregion
+
+	#region UI Actions
+	public void SwitchScreens(int aID)
+	{
+		curScreenID = aID;
+		if(!curAnim)
+		{
+			curAnim = GetComponent<Animator>();
+		}
+		if(curAnim)
+		{
+			curAnim.SetBool("IsActive", false);
+		}
+	}
+
+	public void RestMainMenu()
+	{
+		if(curAnim)
+		{
+			curAnim.SetBool("IsActive", true);
+		}
+	}
+
+	public void AnimSwitch()
+	{
+		Debug.Log ("Reached End of Animation...");
+		switch(curScreenID)
+		{
+			case 0:
+				if(optionsScreen)
+				{
+					optionsScreen.SetActive(true);
+				}
+				break;
+				
+			case 1:
+				if(labScreen)
+				{
+					labScreen.SetActive(true);
+				}
+				break;
+				
+			case 2:
+				if(startScreen)
+				{
+					startScreen.SetActive(true);
+				}
+				break;
+				
+			default:
+				break;
+		}
+	}
+	#endregion
+}
---
+++ Assets/GettingCozy_UI_Video_Assets/Scripts/GT_ScrollerDebug.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GT_ScrollerDebug : MonoBehaviour 
+{
+
+	public ScrollRect scroller;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(scroller)
+		{
+			Debug.Log (scroller.horizontalNormalizedPosition);
+		}
+	}
+}
